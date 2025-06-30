10be73a84cfac09646c2932b06ffcf400c89841c
EndoRodrigo <endoroddrigo19@gmail.com>
2025-06-29 19:54:37 -0500

    feat: create add funcionality shopping cart

---
+++ src/main/java/com/endorodrigo/eComerce/controller/PosController.java
@@ -0,0 +1,41 @@
+package com.endorodrigo.eComerce.controller;
+
+import com.endorodrigo.eComerce.model.Cart;
+import com.endorodrigo.eComerce.model.Product;
+import com.endorodrigo.eComerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
+
+@Controller
+public class PosController {
+
+    Logger logger = Logger.getLogger(String.valueOf(PosController.class));
+
+    @Autowired
+    private ProductService productService;
+
+    private final Cart cart = new Cart();
+
+    @RequestMapping(value = "/pos", method = RequestMethod.GET)
+    public String pos(Model model) {
+        model.addAttribute("cart", cart);
+        return "pos";
+    }
+
+    @RequestMapping(value = "/pos/add", method = RequestMethod.POST)
+    public String addProduct(Model model,@ModelAttribute Product cartForm ) {
+        logger.info("addProduct for id. "+ cartForm.getCode());
+        Product product = productService.findId(cartForm.getCode());
+
+        logger.info("product = " + product);
+        cart.addProduct(product);
+        logger.info("list cart = " + cart);
+        return "redirect:/pos";
+
+    }
+}
