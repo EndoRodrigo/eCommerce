fea428645e3aa8c7f769d30cfd58e013c4fa5478
EndoRodrigo <endoroddrigo19@gmail.com>
2025-07-28 21:24:12 -0500

    Actualización de Customer: nuevos campos y vista para estructura JSON requerida

--- src/main/java/com/endorodrigo/eComerce/model/Customer.java
+++ src/main/java/com/endorodrigo/eComerce/model/Customer.java
@@ -1,5 +1,5 @@
 package com.endorodrigo.eComerce.model;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 /**
@@ -8,17 +8,63 @@ import jakarta.persistence.Entity;
  */
 @Entity
 public class Customer extends Person {
+    @Column(nullable = false)
+    private String identification;
+    private String dv;
+    private String company;
+    private String trade_name;
+    private String names;
     private String address;
+    private String legal_organization_id;
+    private String tribute_id;
+    private Integer identification_document_id;
+    private String municipality_id;
 
     public Customer() {}
+
+    public String getIdentification() { return identification; }
+    public void setIdentification(String identification) { this.identification = identification; }
+
+    public String getDv() { return dv; }
+    public void setDv(String dv) { this.dv = dv; }
+
+    public String getCompany() { return company; }
+    public void setCompany(String company) { this.company = company; }
+
+    public String getTrade_name() { return trade_name; }
+    public void setTrade_name(String trade_name) { this.trade_name = trade_name; }
+
+    public String getNames() { return names; }
+    public void setNames(String names) { this.names = names; }
 
     public String getAddress() { return address; }
     public void setAddress(String address) { this.address = address; }
 
+    public String getLegal_organization_id() { return legal_organization_id; }
+    public void setLegal_organization_id(String legal_organization_id) { this.legal_organization_id = legal_organization_id; }
+
+    public String getTribute_id() { return tribute_id; }
+    public void setTribute_id(String tribute_id) { this.tribute_id = tribute_id; }
+
+    public Integer getIdentification_document_id() { return identification_document_id; }
+    public void setIdentification_document_id(Integer identification_document_id) { this.identification_document_id = identification_document_id; }
+
+    public String getMunicipality_id() { return municipality_id; }
+    public void setMunicipality_id(String municipality_id) { this.municipality_id = municipality_id; }
+
     @Override
     public String toString() {
         return "Customer{" +
-                "address='" + address + '\'' +
+                "identification='" + identification + '\'' +
+                ", dv='" + dv + '\'' +
+                ", company='" + company + '\'' +
+                ", trade_name='" + trade_name + '\'' +
+                ", names='" + names + '\'' +
+                ", address='" + address + '\'' +
+                ", legal_organization_id='" + legal_organization_id + '\'' +
+                ", tribute_id='" + tribute_id + '\'' +
+                ", identification_document_id='" + identification_document_id + '\'' +
+                ", municipality_id='" + municipality_id + '\'' +
                 ", " + super.toString() +
                 '}';
     }
